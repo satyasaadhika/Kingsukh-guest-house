21b77d19a6926181c89187a6fe988885d5d18c65
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2024-09-15 21:37:39 +0530

    Add useEffect hook and scroll animations

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
+import './HeroSection.css';
+
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+function HeroSection(){
+ // useEffect hook to trigger ScrollReveal after component mounts
+ useEffect(() => {
+ //ScrollReveal() is a JavaScript library used to create scroll animations on a webpage.  
+  ScrollReveal().reveal(".hero-container p", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".hero-container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+}, []); 
+  
+  return (
+   <main>
+    <div className="hero-container">
+      <p>Simple - Unique - Friendly</p>
+      <h1>Make Yourself At Home
+      In Our <span>Guest House</span>.</h1>
+    </div>
+
+    <section class="section__container booking__container">
+      <div class="booking__form">
+        <div class="input__group input__btn">
+          <a href="#">
+            <button class="btn">BOOK NOW</button>
+          </a>
+        </div>
+      </div>
+    </section>
+    </main>
+  );
+};
+
+export default HeroSection;
