affce724855dcf6201c4862b514c37b4d0550375
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2024-09-15 16:57:29 +0530

    Add HeroSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HeroSection/>
     </div>
   );
 }
