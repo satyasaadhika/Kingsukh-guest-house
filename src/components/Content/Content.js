235bcab1a65da1fda1df6ef7f52c28d4fc30e40f
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2024-09-29 16:13:53 +0530

    Update content.js

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -43,7 +43,7 @@ ScrollReveal().reveal(".room__card", {
               Escape to comfort in our Cozy Haven Room, a snug retreat designed for intimate relaxation.
             </p>
             <h5>Starting from <span>Rs. 1000/night</span></h5>
-            <a href="https://wa.link/at5ion">
+            <a href="#">
               <button class="btn">BOOK NOW</button>
             </a>
           </div>
@@ -83,7 +83,7 @@ ScrollReveal().reveal(".room__card", {
               Indulge in luxury and ample space in our Spacious Serenity Suite, where tranquility meets roomy elegance.
             </p>
             <h5>Starting from <span>Rs. 1500/night</span></h5>
-            <a href="https://wa.link/at5ion">
+            <a href="#">
               <button class="btn">BOOK NOW</button>
             </a>
           </div>
