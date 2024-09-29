78c995d3cb96b5b61d0e7d56a7d2aadc9c10b65d
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2024-09-29 15:26:07 +0530

    Add booking and customers numbers

---
+++ src/components/Booking/Booking.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Booking.css';
+
+function Booking(){
+    return(
+        <section class="section__container banner__container">
+        <div class="banner__content">
+          <div class="banner__card">
+            <h4>100+</h4>
+            <p>Bookings Completed</p>
+          </div>
+          <div class="banner__card">
+            <h4>150+</h4>
+            <p>Happy Customers</p>
+          </div>
+        </div>
+      </section>
+    );
+};
+export default Booking;
